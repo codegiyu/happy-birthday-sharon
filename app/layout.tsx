2f009ee388bd5a43e72fb473ac1466f1d0563625
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-12-17 22:17:27 +0100

    key added to metas

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,23 +20,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>Happy Birthday Sharon</title>
-        <meta name="description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="birthday message wishes" />
-        <meta name="author" content="Edward-Precious Omegbu Codegiyu" />
-        <meta property="og:type" content="message" />
-        <meta property="og:title" content="Happy Birthday Sharon" />
-        <meta property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <meta property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
-        <meta name="twitter:card" content="message" />
-        <meta name="twitter:title" content="Happy Birthday Sharon" />
-        <meta name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
-        <meta name="twitter:site" content="@TheLonerider20" />
-        <meta name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <link rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
+        <title key={"new"}>Happy Birthday Sharon</title>
+        <meta key={"new"} name="description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta key={"new"} name="robots" content="index, follow" />
+        <meta key={"new"} name="keywords" content="birthday message wishes" />
+        <meta key={"new"} name="author" content="Edward-Precious Omegbu Codegiyu" />
+        <meta key={"new"} property="og:type" content="message" />
+        <meta key={"new"} property="og:title" content="Happy Birthday Sharon" />
+        <meta key={"new"} property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta key={"new"} property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <meta key={"new"} property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta key={"new"} name="twitter:card" content="message" />
+        <meta key={"new"} name="twitter:title" content="Happy Birthday Sharon" />
+        <meta key={"new"} name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta key={"new"} name="twitter:site" content="@TheLonerider20" />
+        <meta key={"new"} name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta key={"new"} name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <link key={"new"} rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
       </head>
       <body className={montserrat.className}>
         {children}
