aee4467854e31e75e789f37295cf06540f003943
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-12-17 21:29:37 +0100

    canonical url updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,14 +29,14 @@ export default function RootLayout({
         <meta property="og:title" content="Happy Birthday Sharon" />
         <meta property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
         <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <meta property="og:url" content="https://happy-birthday-sharon.vercel.app" />
+        <meta property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
         <meta name="twitter:card" content="message" />
         <meta name="twitter:title" content="Happy Birthday Sharon" />
-        <meta name="twitter:url" content="https://happy-birthday-sharon.vercel.app" />
+        <meta name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
         <meta name="twitter:site" content="@TheLonerider20" />
         <meta name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
         <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <link rel="canonical" href="https://happy-birthday-sharon.vercel.app" />
+        <link rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
       </Head>
       <body className={montserrat.className}>
         {children}
