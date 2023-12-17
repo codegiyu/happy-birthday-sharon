b5823a05ae7483788c7d1ea48f752572cfbfefd7
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-12-17 21:57:47 +0100

    head tag added in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="birthday message wishes" />
+        <meta name="author" content="Edward-Precious Omegbu Codegiyu" />
+        <meta property="og:type" content="message" />
+        <meta property="og:title" content="Happy Birthday Sharon" />
+        <meta property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <meta property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta name="twitter:card" content="message" />
+        <meta name="twitter:title" content="Happy Birthday Sharon" />
+        <meta name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta name="twitter:site" content="@TheLonerider20" />
+        <meta name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <link rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
+      </head>
       <body className={montserrat.className}>
         {children}
 
