45bf1514e625107d4746b05d25938a372c36eaa3
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-12-17 21:38:17 +0100

    head metadata updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head'
 
 const montserrat = Montserrat({
   weight: ["300", "400", "500", "600", "700"],
@@ -20,24 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head key={"my-head"}>
-        <meta name="description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta name="robots" content="index, follow" />
-        <meta name="keywords" content="birthday message wishes" />
-        <meta name="author" content="Edward-Precious Omegbu Codegiyu" />
-        <meta property="og:type" content="message" />
-        <meta property="og:title" content="Happy Birthday Sharon" />
-        <meta property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <meta property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
-        <meta name="twitter:card" content="message" />
-        <meta name="twitter:title" content="Happy Birthday Sharon" />
-        <meta name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
-        <meta name="twitter:site" content="@TheLonerider20" />
-        <meta name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
-        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
-        <link rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
-      </Head>
       <body className={montserrat.className}>
         {children}
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import FireworksScreen from "@/screens/FireworksScreen";
 import MessageScreen from "@/screens/MessageScreen";
 import WelcomeScreen from "@/screens/WelcomeScreen";
 import useAppStore from "@/store/useAppStore";
+import Head from "next/head";
 import { useEffect } from "react";
 
 
@@ -21,6 +22,25 @@ export default function Home() {
 
   return (
     <main className="w-full h-screen bg-black-0a ralative overflow-hidden">
+      <Head key={"my-head"}>
+        <meta name="description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="birthday message wishes" />
+        <meta name="author" content="Edward-Precious Omegbu Codegiyu" />
+        <meta property="og:type" content="message" />
+        <meta property="og:title" content="Happy Birthday Sharon" />
+        <meta property="og:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta property="og:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <meta property="og:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta name="twitter:card" content="message" />
+        <meta name="twitter:title" content="Happy Birthday Sharon" />
+        <meta name="twitter:url" content="https://happy-birthday-sharon-six.vercel.app" />
+        <meta name="twitter:site" content="@TheLonerider20" />
+        <meta name="twitter:description" content="Happy 19th birthday to my dear friend Sharon" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/diirhfihi/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1702843441/sharon-favicon_gfkwki.jpg?_s=public-apps" />
+        <link rel="canonical" href="https://happy-birthday-sharon-six.vercel.app" />
+      </Head>
+
       {currentScreen === "welcome" ? (
         <div className="w-full h-screen absolute top-0 left-0 z-1">
           <WelcomeScreen />
